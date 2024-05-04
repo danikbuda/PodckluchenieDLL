cd7fe6440ca1ee5cc72ba461d1c05333cb13e9b8
buda danic <danikbuda@outlook.com>
2024-05-04 21:12:12 +0700

    добавление исходных файлов DLL библиотеки

---
+++ DynamicLinkLibrary/DynamicLinkLibrary/DynamicLL.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodckluchenieDLL
+{
+    public static class DynamicLL
+    {
+        public static double Add(double firstValue, double secondValue) 
+        {
+            return firstValue + secondValue;
+        }
+        public static double Substract(double firstValue, double secondValue)
+        {
+            return firstValue - secondValue;
+        }
+        public static double Multiply(double firstValue, double secondValue)
+        {
+            return firstValue * secondValue;
+        }
+        public static double Divide(double firstValue, double secondValue)
+        {
+            if (secondValue == 0)
+                return 0;
+            return firstValue / secondValue;
+        }
+
+
+
+    }
+}
